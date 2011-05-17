d8606d3d3f0017191aa5f0eba9ef2bad7a8c961e
Michal Špaček <tupinek@gmail.com>
2011-05-18 00:26:49 +0200

    Rewrite to use Error::Pure::Utils.

--- Pure/AllError.pm
+++ Pure/AllError.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 
 # Modules.
-use Error::Pure qw(err_helper);
+use Error::Pure::Utils qw(err_helper);
 use Error::Pure::Output::Text qw(err_bt_pretty);
 use List::MoreUtils qw(none);
 use Readonly;
