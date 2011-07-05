510d3a5833143eab4a80b55df523745dd9b5bfbc
Michal Špaček <tupinek@gmail.com>
2011-07-05 22:07:17 +0200

    Rewrite to better version. Add doc.

--- Pure/ErrorList.pm
+++ Pure/ErrorList.pm
@@ -8,10 +8,12 @@ use warnings;
 # Modules.
 use Error::Pure::Utils qw(err_helper);
 use Error::Pure::Output::Text qw(err_bt_simple);
+use List::MoreUtils qw(none);
 use Readonly;
 
 # Constants.
 Readonly::Array our @EXPORT_OK => qw(err);
+Readonly::Scalar my $EVAL => 'eval {...}';
 
 # Version.
 our $VERSION = 0.01;
@@ -21,25 +23,134 @@ $SIG{__DIE__} = 'IGNORE';
 
 # Process error.
 sub err {
+	my @msg = @_;
 
-	# Arguments.
-	my $msg = \@_;
-
-	# Errors.
-	my $errors = err_helper(@{$msg});
+	# Get errors structure.
+	my $errors_ar = err_helper(\@msg);
 
 	# Finalize in main on last err.
-	my $stack = $errors->[-1]->{'stack'};
-	if ($stack->[-1]->{'class'} eq 'main'
-		&& ! grep({ $_ eq 'eval {...}' || $_ =~ /^eval '/} 
-		map { $_->{'sub'} } @{$stack})) {
+	my $stack_ar = $errors_ar->[-1]->{'stack'};
+	if ($stack_ar->[-1]->{'class'} eq 'main'
+		&& none { $_ eq $EVAL || $_ =~ /^eval '/ms }
+		map { $_->{'sub'} } @{$stack_ar}) {
 
-		die Error::Pure::Output::Text::err_bt_simple($errors);
+		die err_bt_simple($errors_ar);
 
 	# Die for eval.
 	} else {
-		die "$msg->[0]\n";
+		my $e = $errors_ar->[-1]->{'msg'}->[0];
+		chomp $e;
+		die "$e\n";
 	}
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Error::Pure::ErrorList - Error::Pure class with list of errors in one line
+with informations.
+
+=head1 SYNOPSIS
+
+ use Error::Pure::ErrorList qw(err);
+ err "This is a fatal error.", "name", "value";
+
+=head1 SUBROUTINES
+
+=over 4
+
+=item B<err(@messages)>
+
+ Process error with messages @messages.
+
+=back
+
+=head1 EXAMPLE1
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Error::Pure::ErrorList qw(err);
+
+ # Error.
+ err '1';
+
+ # Output:
+ # #Error [example1.pl:9] 1
+
+=head1 EXAMPLE2
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Error::Pure::ErrorList qw(err);
+
+ # Error.
+ err '1', '2', '3';
+
+ # Output:
+ # #Error [example2.pl:9] 1
+
+=head1 EXAMPLE3
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use English qw(-no_match_vars);
+ use Error::Pure::ErrorList qw(err);
+
+ # Error.
+ eval { err "1"; };
+ if ($EVAL_ERROR) {
+        err "2";
+ }
+
+ # Output:
+ # #Error [example3.pl:10] 1
+ # #Error [example3.pl:11] 2
+
+=head1 DEPENDENCIES
+
+L<Error::Pure::Utils(3pm)>,
+L<Error::Pure::Output::Text(3pm)>,
+L<Exporter(3pm)>,
+L<List::MoreUtils(3pm)>,
+L<Readonly(3pm)>.
+
+=head1 SEE ALSO
+
+L<Error::Pure(3pm)>,
+L<Error::Pure::AllError(3pm)>,
+L<Error::Pure::Clean(3pm)>,
+L<Error::Pure::die(3pm)>,
+L<Error::Pure::Error(3pm)>,
+L<Error::Pure::Output::Text(3pm)>,
+L<Error::Pure::Print(3pm)>,
+L<Error::Pure::Utils(3pm)>.
+
+=head1 AUTHOR
+
+Michal Špaček L<skim@cpan.org>
+
+=head1 LICENSE AND COPYRIGHT
+
+BSD license.
+
+=head1 VERSION
+
+0.01
+
+=cut
