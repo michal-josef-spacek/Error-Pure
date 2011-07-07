314fa5f141b311a02391342a6d460abe0b7144c3
Michal Špaček <tupinek@gmail.com>
2011-07-07 15:37:53 +0200

    Remove TODO in doc SYNOPSIS section.

--- Pure/Utils.pm
+++ Pure/Utils.pm
@@ -176,7 +176,7 @@ Error::Pure::Utils - Utilities for structured errors.
 =head1 SYNOPSIS
 
  use Error::Pure::Utils qw(clean err_get err_helper);
- TODO
+ clean();
  my @ret = err_get(1);
  my $errors_ar = err_helper('This is a fatal error', 'name', 'value');
 
