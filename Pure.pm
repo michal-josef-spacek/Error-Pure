bbf9d2778213159250912765d0489edf3441ccf6
Michal Josef Špaček <tupinek@gmail.com>
2018-01-22 13:04:39 +0100

    Update copyright years

--- Pure.pm
+++ Pure.pm
@@ -241,7 +241,7 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2008-2016 Michal Josef Špaček
+ © 2008-2018 Michal Josef Špaček
  BSD 2-Clause License
 
 =head1 VERSION
--- Pure/Error.pm
+++ Pure/Error.pm
@@ -125,7 +125,7 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2008-2017 Michal Josef Špaček
+ © 2008-2018 Michal Josef Špaček
  BSD 2-Clause License
 
 =head1 VERSION
